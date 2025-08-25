064d34fbded823ed32633a3894dfe99641da9c98
Abdurakhmon <abdurakhmonxamdamov@gmail.com>
2025-08-26 00:31:57 +0500

    home page is updated added some futures and designes

--- blog/views.py
+++ blog/views.py
@@ -6,19 +6,19 @@ posts = [
   {
     'author': 'Jim Kin',
     'title': 'How to get 4.5 GPA',
-    'content': 'You need to work and study so hard',
+    'content': 'You need to work and study so hard, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel,',
     'date': 'August 25th 2025'
   },
   {
     'author': 'Rasul',
     'title': 'Qanday Unutay menga ham o\'rgat',
-    'content': 'Nasini emsin',
+    'content': 'Nasini emsin, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel,',
     'date': 'August 25th 2025'
   },
   {
     'author': 'Jim Kin',
     'title': 'How to get 4.5 GPA',
-    'content': 'I don\'t know',
+    'content': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, ',
     'date': 'August 25th 2025'
   },
 
