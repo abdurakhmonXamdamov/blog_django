6b3e958159247164137eee777bbceb1fb4b45ed9
Abdurakhmon <abdurakhmonxamdamov@gmail.com>
2025-09-20 22:37:20 +0500

    Promlem in register page is fixed, login and register page upgraded and be more informative

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,49 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.contrib.auth.forms import AuthenticationForm # for login purposes
 
 class UserCustomForm(UserCreationForm):
   # first_name = forms.CharField(max_length=30, required=True)
   # last_name = forms.CharField(max_length=30, required=True)
-  email = forms.EmailField()
+  email = forms.EmailField(
+    widget= forms.EmailInput(attrs={
+      'class': 'form-control',
+      'placeholder': 'Enter your valid email'
+    })
+  )
+
+  username = forms.CharField(
+    widget= forms.TextInput(attrs={
+      'class': 'form-control',
+      'placeholder': 'Enter unique Username'
+    })
+  )
+
+  password1 = forms.CharField(
+    widget= forms.PasswordInput(attrs={
+      'class' : 'form-control',
+      'placeholder': 'Enter your password'
+    })
+  )
+
+  password2 = forms.CharField(
+    widget= forms.PasswordInput(attrs={
+      'class' : 'form-control',
+      'placeholder': 'Confirm your password'
+    })
+  )
 
   class Meta:
     model = User
     fields = ['username', 'email', 'password1', 'password2']
 
-  # def save(self, commit=True):
-  #       user = super().save(commit=False)
-  #       user.username = self.cleaned_data['first_name']
-  #       if commit:
-  #           user.save()
-  #       return user
 
+
+class UserLoginForm(AuthenticationForm):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    for name, field in self.fields.items():
+      field.widget.attrs['class'] = 'form-control'
+      field.widget.attrs['placeholder'] = f'Enter {field.label}'
--- users/views.py
+++ users/views.py
@@ -3,14 +3,17 @@ from django.contrib import messages
 from .forms import UserCustomForm
 from django.contrib.auth.decorators import login_required
 
+
 def register(request):
   if request.method == 'POST':
     form = UserCustomForm(request.POST)
-    if form.is_valid:
+    if form.is_valid():
         form.save()
-        username = form.cleaned_data.get('first_name')
+        username = form.cleaned_data.get('username')
         messages.success(request, f'Your account has been created, You can now log in!')
         return redirect('login')
+    else:
+      print(form.errors)
   else:
       form = UserCustomForm()
   return render(request, 'users/register.html', {'form': form})
@@ -18,6 +21,8 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+
 
 
 # we got different types of messages:
