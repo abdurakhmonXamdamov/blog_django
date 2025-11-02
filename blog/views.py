fc5da9b07cb38e96fc2a11b524f73017c75b7e70
Abdurakhmon <abdurakhmonxamdamov@gmail.com>
2025-11-02 08:50:20 +0500

    post class based view created

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Posts
 
 # Create your views here.
 
-def home(request):
-  context = {
-    'posts': Posts.objects.all()
-  }
-  return render(request, 'blog/home.html', context)
+class PostListView(ListView):
+  model = Posts
+  template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+  context_object_name = 'posts'
+  ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+  model = Posts
+  template_name = 'blog/full_post.html' # <app>/<model>_<viewtype>.html
+  # context_object_name = 'posts'
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
 
-def posts(request):
-  return render(request, 'blog/full_post.html')
